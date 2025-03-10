2ab268e14ecd91a87a56583ffe8456a8cbf90a5f
MaritS23 <maritschiller23@gmail.com>
2025-03-10 13:20:14 +0100

    Fanger einegführt

--- Catch-the-Ball.py
+++ Catch-the-Ball.py
@@ -17,18 +17,38 @@ WIN = pygame.display.set_mode ((BREITE, HOEHE))
 
 FPS = 60
 
-def draw (win):
-    win.fill(("WHITE"))
+FANGER_BREITE = 100
+FANGER_HOEHE = 20
 
+class Fanger:
+    Fanger_Farbe = ("BLACK")
+    GES = 7
+
+    def __init__ (self, x, y, fanger_breite, fanger_hoehe):
+        self.x = x
+        self.y = y
+        self.fanger_breite = fanger_breite
+        self.fanger_hoehe = fanger_hoehe
+
+    def draw (self, win):
+        pygame.draw.rect (win,self.Fanger_Farbe, (self.x, self.y, self.fanger_breite, self.fanger_hoehe) )
+
+
+def draw (win,fanger):
+    win.fill(("WHITE"))
+    fanger.draw(win)
     pygame.display.update()
 
 def main():
 
     run=True 
     clock = pygame.time.Clock()
-    draw(WIN)
-    
+    fanger = Fanger(BREITE/2 - FANGER_BREITE/2, HOEHE - 100, FANGER_BREITE, FANGER_HOEHE)
+
+
+
     while run:
+        draw(WIN, fanger)
         clock.tick(FPS)
 
         for event in pygame.event.get():
